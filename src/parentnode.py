d1e615e1a00469cbb5539741064c45316dcb57bc
Meeman <meeman_3@outlook.com>
2025-09-02 01:14:10 +0100

    added parentnodes

---
+++ src/parentnode.py
@@ -0,0 +1,22 @@
+from src.htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("no tag")
+        
+        if self.children == None:
+            raise ValueError("no children")
+        
+        html_string = f"<{self.tag}{self.props_to_html()}>"
+
+        for child in self.children:
+            html_string += child.to_html()
+
+        html_string += f"</{self.tag}>"
+
+        return html_string
