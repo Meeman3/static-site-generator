e801357e76a7742fa2fa266c46aa5a5111e19a9e
Meeman <meeman_3@outlook.com>
2025-09-13 16:42:03 +0100

    fixed regex and added test, changed splitnode file

--- src/inline_md_funcs.py
+++ src/inline_md_funcs.py
@@ -1,13 +1,45 @@
 import re
+from src.textnode import TextNode, TextType
 
 def extract_markdown_images(text):
 
-    imgs = re.findall(r"\[(.*?)\]\((.*?\/\/.*?\..*?\))")
+    imgs = re.findall(r"!\[(.*?)\]\((.*?\/\/.*?\..*?)\)", text)
 
     return imgs
 
-def extract_makdown_links(links):
+def extract_markdown_links(links):
 
-    alt_text_urls = re.findall(r"!\[(.*?)\]\((.*?\/\/.*?\..*?\))")
+    alt_text_urls = re.findall(r"\[(.*?)\]\((.*?\/\/.*?\..*?)\)", links)
 
     return alt_text_urls
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    if delimiter not in {"**", "_", "`"}:
+        raise Exception("invalid markdown syntax")
+    
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        split_node = node.text.split(delimiter)
+
+        if len(split_node) % 2 == 0:
+            raise Exception("markdown syntax not closed")
+        
+        if len(split_node) == 1:
+            new_nodes.append(node)
+        
+        for i, split in enumerate(split_node):
+            if split == "":
+                continue
+            elif i % 2:
+                new_nodes.append(TextNode(f"{split}", text_type))
+            else:
+                new_nodes.append(TextNode(f"{split}", TextType.TEXT))
+
+    
+    return new_nodes
