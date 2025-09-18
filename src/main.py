6fa966ec507eb489f87376b0d06ac489e76b4394
Meeman <meeman_3@outlook.com>
2025-09-18 22:17:09 +0100

    added dir copy func

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
-from textnode import TextNode
+
+import os
+import shutil
 
 
 def main():
 
-    node = TextNode("dummy text", "text_plain")
+    dir_copy("./static/", "./public/")
 
-    print(node)
+def dir_copy(src_dir, dst_dir):
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+
+    
+    src_path = src_dir
+    dst_path = dst_dir
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dst_path = os.path.join(dst_dir, item)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+            
+        else:
+            dir_copy(src_path, dst_path)
+
+
+
+
+
 
 
 main()
