15baeb39f4ba0fea3d1a7ea8d04d479284636a9f
Meeman <meeman_3@outlook.com>
2025-09-18 17:15:07 +0100

    added split img and links inline funcs

--- src/inline_md_funcs.py
+++ src/inline_md_funcs.py
@@ -43,3 +43,77 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        old_text = old_node.text
+        imgs = extract_markdown_images(old_text)
+
+        if len(imgs) == 0:
+            new_nodes.append(old_node)
+
+        for img in imgs:
+            split_txt = old_text.split(f"![{img[0]}]({img[1]})", 1)
+
+            if len(split_txt) != 2:
+                raise ValueError("invalid markdown image")
+            
+            if split_txt[0] != "":
+                new_nodes.append(TextNode(split_txt[0], TextType.TEXT))
+
+            new_nodes.append(
+                TextNode(
+                f"{img[0]}", TextType.IMAGE, f"{img[1]}"
+                )
+            )
+
+            old_text = split_txt[1]
+
+        if old_text != "":
+            new_nodes.append(TextNode(old_text, TextType.TEXT))
+        
+        return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        old_text = old_node.text
+        links = extract_markdown_links(old_text)
+
+        if len(links) == 0:
+            new_nodes.append(old_node)
+
+        for link in links:
+            split_txt = old_text.split(f"[{link[0]}]({link[1]})", 1)
+
+            if len(split_txt) != 2:
+                raise ValueError("invalid markdown image")
+            
+            if split_txt[0] != "":
+                new_nodes.append(TextNode(split_txt[0], TextType.TEXT))
+
+            new_nodes.append(
+                TextNode(
+                f"{link[0]}", TextType.LINK, f"{link[1]}"
+                )
+            )
+
+            old_text = split_txt[1]
+
+        if old_text != "":
+            new_nodes.append(TextNode(old_text, TextType.TEXT))
+        
+
+    return new_nodes
